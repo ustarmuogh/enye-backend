0b6059f71e51409015b8501ff01e71ed6f3721bd
ustarmuogh <ustarmuogh@gmail.com>
2021-01-16 08:31:31 +0100

    chore(setup):initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const axios = require('axios').default;
+
+const app = express();
+
+const PORT = '8050';
+
+app.get('/api/rates', async (req, res) => {
+    const { query } = req;
+
+    let currencies = [];
+
+    const date = new Date();
+    const formattedDate = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+
+    try {
+        const response = await axios.get(`https://api.exchangeratesapi.io/latest?base=${query.base}`);
+
+        let rates = {};
+
+        if (query.currency) {
+            currencies = query.currency.split(',');
+
+            currencies.forEach(currency => {
+                rates[currency] = response.data.rates[currency]
+            });
+        } else {
+            rates = response.data.rates;
+        }
+
+        res.status(200).json({
+            results: {
+                base: query.base,
+                date: formattedDate,
+                rates,
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Something went wrong'
+        });
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`);
+});
